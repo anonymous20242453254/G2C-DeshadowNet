464cf5ae31200c5012f20b1adb393523c0ffb3d9
anonymous20242453254 <98793215+anonymous20242453254@users.noreply.github.com>
2022-02-04 00:34:52 +0800

    Create __init__.py

---
+++ models/__init__.py
@@ -0,0 +1,23 @@
+
+def find_model_using_name(model_name):
+    model_filename = "models." + model_name + "_model"
+    modellib = importlib.import_module(model_filename)
+    model = None
+    target_model_name = model_name.replace('_', '') + 'model'
+    for name, cls in modellib.__dict__.items():
+        if name.lower() == target_model_name.lower() \
+           and issubclass(cls, BaseModel):
+            model = cls
+    return model
+
+
+def get_option_setter(model_name):
+    model_class = find_model_using_name(model_name)
+    return model_class.modify_commandline_options
+
+
+def create_model(opt):
+    model = find_model_using_name(opt.model)
+    instance = model(opt)
+    print("model [%s] was created" % type(instance).__name__)
+    return instance
